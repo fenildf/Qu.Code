307a5845e565c91945621d65a5ee93b0239d84e0
vo01github <hxt2014bbg@icloud.com>
2015-06-14 15:41:56 +0800

    1

---
+++ 在线题库题解/README.md
@@ -0,0 +1,26 @@
+# 说明
+
+## LintCode 
+
+题解 [github 上的答案，有pdf]
+>
+https://github.com/soulmachine/lintcode
+
+LintCode题解，LintCode是LeetCode.com网站的增强版，题目更优质，覆盖知识点更多 
+
+好像是国人做的网站
+>
+http://www.lintcode.com 
+
+
+
+
+## LeetCode
+题解 [github 上的答案，有pdf]
+>
+https://github.com/soulmachine/leetcode
+
+题目
+>
+https://leetcode.com
+
---
+++ 题库/数据结构-线性结构/数组/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,62 @@
+﻿
+
+// 2.1.1 
+
+// 题目来源: http://www.lintcode.com/zh-cn/problem/remove-duplicates-from-sorted-array/
+
+// __________________________ Remove Duplicates from Sorted Array __________________________
+/*
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+Example
+
+Given input array A = [1,1,2],
+
+Your function should return length = 2, and A is now [1,2].
+
+*/
+
+// __________________________ Tags __________________________
+/*
+	Two Pointers , Array
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1
+// 时间复杂度O(n), 空间复杂度O(1)
+
+class Solution {
+public:
+    /**
+     * @param A: a list of integers
+     * @return : return an integer
+     */
+    int removeDuplicates(vector<int> &nums) {
+        // write your code here
+        const int size = nums.size();
+        if (size == 0)return 0;
+
+        int index = 0;
+        for (int i = 1; i < size; ++i) {
+            if (nums[i] != nums[index])
+                nums[++index] = nums[i];
+        }
+        
+        return index + 1;
+    }
+};
+
+
+// 代码2
+// 时间复杂度O(n)，空间复杂度O(1)
+class Solution {
+public:
+    int removeDuplicates(vector<int> &A) {
+        return distance(A.begin(), unique(A.begin(), A.end()));
+    }
+};
+
+
