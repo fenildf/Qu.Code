6b4aaf076585d32c63b46c014fd5acf747f34ff2
vo01github <hxt2014bbg@icloud.com>
2015-06-26 22:29:22 +0800

    1

---
+++ 题库/数据结构-树结构/二叉树/Lowest Common Ancestor.cpp
@@ -0,0 +1,86 @@
+﻿
+
+
+// __________________________ Lowest Common Ancestor __________________________
+/*
+题目来源:
+http://www.lintcode.com/en/problem/lowest-common-ancestor/
+
+二叉树的层次遍历 II
+*/  
+
+
+/*
+Given the root and two nodes in a Binary Tree. Find the lowest common ancestor(LCA) of the two nodes.
+
+The lowest common ancestor is the node with largest depth which is the ancestor of both nodes.
+
+
+Example
+For the following binary tree:
+
+  4
+ / \
+3   7
+   / \
+  5   6
+LCA(3, 5) = 4
+
+LCA(5, 6) = 7
+
+LCA(6, 7) = 7
+*/
+
+// __________________________ Tags __________________________
+/*
+	 LintCode 自己出的题目...
+*/
+
+// __________________________ 所用到的数据结构 __________________________
+ /**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+// __________________________ 实现细节 __________________________ 
+// 代码1 - 递归版
+// 辅助空间-
+
+
+class Solution {
+public:
+    /**
+     * @param root: The root of the binary search tree.
+     * @param A and B: two nodes in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
+    TreeNode *target; 
+    bool init;
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *A, TreeNode *B) {
+        // write your code here
+        target = nullptr;
+        findNode(root,A,B);
+        return target;
+    }
+    
+    int findNode(TreeNode *root, TreeNode *A, TreeNode *B) {
+        // write your code here
+        if (root == nullptr) return 0;
+        int temp = 0;
+        if (root == A) temp += 1;       //预防这种输入：{1}, 1, 1
+        if (root == B) temp += 1;
+        temp += findNode(root->left,A,B) + findNode(root->right,A,B);
+        
+        if (temp == 2 && target == nullptr) target = root;
+            
+        return temp;
+    }
+};
---
+++ 题库/数据结构-树结构/二叉树/Validate Binary Search Tree.cpp
@@ -0,0 +1,93 @@
+﻿
+
+
+// __________________________ Lowest Common Ancestor __________________________
+/*
+题目来源:
+https://leetcode.com/problems/validate-binary-search-tree/
+http://www.lintcode.com/zh-cn/problem/validate-binary-search-tree/
+
+验证二叉查找树
+*/  
+
+
+/*
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+1-The left subtree of a node contains only nodes with keys less than the node's key.
+2-The right subtree of a node contains only nodes with keys greater than the node's key.
+3-Both the left and right subtrees must also be binary search trees.
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+
+给定一个二叉树，判断它是否是合法的二叉查找树(BST)
+一棵BST定义为：
+
+节点的左子树中的值要严格小于该节点的值。
+节点的右子树中的值要严格大于该节点的值。
+左右子树也必须是二叉查找树。
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Tree, Depth-first Search
+*/
+
+// __________________________ 所用到的数据结构 __________________________
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// __________________________ 实现细节 __________________________ 
+// 代码1 - 递归版
+// 辅助空间-
+// 传递的值，需要把最大，最小值都传递回来才行，然后进行对比。
+
+class Solution {
+public:
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    bool isBST; 
+    bool isValidBST(TreeNode *root) {
+        // write your code here
+        isBST = true;
+        compare(root, 0);
+        return isBST;
+    }
+    
+    /*
+        i:  0-说明是左子树，返回左子树节点中max的值; 
+            1-说明是右子树，返回右子树节点中min的值; 
+    */ 
+    int compare(TreeNode *root,int i) {
+        // write your code here
+        if(root == nullptr)return 0;
+        int mid = root->val;
+        
+        if (root->left != nullptr) 
+        {
+            int left = compare(root->left, 0);
+            if (left >= root->val) isBST = false;
+            if (i == 1) mid = left;
+        }
+        
+        if (root->right != nullptr) 
+        {
+            int right = compare(root->right, 1);
+            if (right <= root->val) isBST = false;
+            if (i == 0) mid = right;
+        }
+            
+        return mid;
+    }
+};
