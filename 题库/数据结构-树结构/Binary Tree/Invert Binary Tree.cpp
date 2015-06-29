d2e5c5653f3e47e0794968f3cf33fb27c1723419
vo01github <hxt2014bbg@icloud.com>
2015-06-30 01:53:08 +0800

    1

---
+++ 题库/数据结构-树结构/Binary Tree/Invert Binary Tree.cpp
@@ -0,0 +1,75 @@
+﻿
+
+
+// __________________________ Invert Binary Tree __________________________
+/*
+题目来源:
+https://leetcode.com/problems/invert-binary-tree/
+http://www.lintcode.com/zh-cn/problem/invert-binary-tree/
+
+知乎热议讨论帖：http://www.zhihu.com/question/31202353?rf=31187043
+
+翻转二叉树
+*/  
+
+
+/*
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew), 
+but you can’t invert a binary tree on a whiteboard so fuck off.
+
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Tree,
+*/
+
+// __________________________ 所用到的数据结构 __________________________
+ /**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+// __________________________ 实现细节 __________________________ 
+// 代码1 - 递归版
+// 辅助空间-
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) return root;
+        TreeNode * temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        return root;
+    }
+};
---
+++ 题库/数据结构-线性结构/链表/Delete Node in the Middle of Singly Linked List.cpp
@@ -0,0 +1,42 @@
+﻿
+
+
+// __________________________ Delete Node in the Middle of Singly Linked List  __________________________
+/*
+题目来源:
+
+http://www.lintcode.com/en/problem/delete-node-in-the-middle-of-singly-linked-list/
+
+*/  
+
+
+/*
+Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node.
+
+
+Example
+Given 1->2->3->4, and node 3. return 1->2->4
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Linked List
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1
+// 时间复杂度O(1),空间复杂度O(1)
+class Solution {
+public:
+    /**
+     * @param node: a node in the list should be deleted
+     * @return: nothing
+     */
+    void deleteNode(ListNode *node) {
+        // write your code here
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
