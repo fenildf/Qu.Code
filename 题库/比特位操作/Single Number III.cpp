22e290bb7be121184d2ec2f638b29b4184964127
vo01github <hxt2014bbg@icloud.com>
2015-07-04 19:35:09 +0800

    2

---
+++ 题库/比特位操作/Single Number III.cpp
@@ -0,0 +1,68 @@
+﻿
+
+
+// __________________________ Single Number III  __________________________
+/*
+题目来源:
+
+http://www.lintcode.com/en/problem/single-number-iii/
+
+*/  
+
+
+/*
+Given 2*n + 2 numbers, every numbers occurs twice except two, find them.
+
+Have you met this question in a real interview? Yes
+Example
+Given [1,2,2,3,4,4,5,3] return 1 and 5
+
+Challenge
+O(n) time, O(1) extra space.
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Bit Manipulation 
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1 - 使用 异或的 特殊属性。
+// 时间复杂度O(2n),空间复杂度O(1)
+/*
+    对于任何数x，都有x^x=0，x^0=x。
+    异或运算 满足 交换律。 a^b^c = a^c^b
+*/ 
+class Solution {
+public:
+    /**
+     * @param A : An integer array
+     * @return : Two integers
+     */
+    vector<int> singleNumberIII(vector<int> &nums) {
+        // write your code here
+        int xorSum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            xorSum = xorSum ^ nums[i];
+        }
+
+        // 输出 xorSum 的低位中的第一个 1 的位置
+        int firstOneBit = xorSum & ~(xorSum - 1);   // 在这位上，a和b 不一样，一个为1，一个为0，就这样把a和b过滤出来。
+
+        int a = 0;
+        for (int j = 0; j < nums.size(); ++j) {
+            if (nums[j] & firstOneBit) {
+                a ^= nums[j];       // 把B过滤了，因为B在这位为0
+            }
+        }
+        
+        int b = xorSum ^ a;
+        
+        vector<int> singleOne;
+        singleOne.push_back(a);
+        singleOne.push_back(b);
+        
+        return singleOne;
+    }
+};
---
+++ 题库/比特位操作/Single Number.cpp
@@ -0,0 +1,63 @@
+﻿
+
+
+// __________________________ Single Number  __________________________
+/*
+题目来源:
+
+http://www.lintcode.com/zh-cn/problem/single-number/
+https://leetcode.com/problems/single-number/
+
+*/  
+
+
+/*
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Bit Manipulation ，Hash Table
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1 - 使用 异或的 特殊属性。
+// 时间复杂度O(n),空间复杂度O(1)
+/*
+    对于任何数x，都有x^x=0，x^0=x。
+    异或运算 满足 交换律。 a^b^c = a^c^b
+*/ 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xorSum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            xorSum = xorSum ^ nums[i];
+        }
+        
+        return xorSum;
+    }
+};
+
+
+/*
+    google面试题的变形：一个数组存放若干整数，一个数出现奇数次，其余数均出现偶数次，
+    找出这个出现奇数次的数？
+
+    同样用 上面的代码  int singleNumber(vector<int>& nums)  来计算。
+*/
+
+
+/*
+    1-1000放在含有1001个元素的数组中，只有唯一的一个元素值重复，其它均只出现一次。
+    每个数组元素只能访问一次，设计一个算法，将它找出来；
+    不用辅助存储空间，能否设计一个算法实现？
+
+    long sum = (1+1000)×1000/2 = 500500;
+    int num = sum - A[i](0~1000)    //  得到了要找的元素的 负值。
+    return (-1)*num;
+*/    
