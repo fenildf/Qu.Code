49ff21911f002d76b5f093dc5e56e80f6e333aa5
vo01github <hxt2014bbg@icloud.com>
2015-07-05 19:55:23 +0800

    2

---
+++ 题库/二分法/Sqrt(x).cpp
@@ -0,0 +1,54 @@
+﻿
+
+
+// __________________________ Sqrt(x)  __________________________
+/*
+题目来源:
+
+http://www.lintcode.com/en/problem/sqrtx/
+
+*/  
+
+
+/*
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
+Have you met this question in a real interview? Yes
+Example
+sqrt(3) = 1
+
+sqrt(4) = 2
+
+sqrt(5) = 2
+
+sqrt(10) = 3
+
+Challenge
+O(log(x))
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Binary Search
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1 - 使用STL
+// 时间复杂度O(n),空间复杂度O(1)
+class Solution {
+public:
+    /**
+     * @param x: An integer
+     * @return: The sqrt of x
+     */
+    int sqrt(int number) {
+        // write your code here
+
+        return sqrtf(number); 
+    }
+};
+
+// 代码2 - 使用二分法
---
+++ 题库/比特位操作/A + B Problem.cpp
@@ -0,0 +1,66 @@
+﻿
+
+
+// __________________________ A + B Problem  __________________________
+/*
+题目来源:
+http://www.lintcode.com/en/problem/a-b-problem/
+
+*/  
+
+
+/*
+Write a function that add two numbers A and B. You should not use + or any arithmetic operators.
+
+
+Example
+Given a=1 and b=2 return 3
+
+Note
+There is no need to read data from standard input stream. 
+Both parameters are given in function aplusb, you job is to calculate the sum and return it.
+
+Challenge
+Of course you can just return a + b to get accepted. But Can you challenge not do it like that?
+
+Clarification
+Are a and b both 32-bit integers?
+
+Yes.
+Can I use bit operation?
+
+Sure you can.
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Bit Manipulation 
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1 - 
+// 时间复杂度O(n),空间复杂度O(1)
+class Solution {
+public:
+    /*
+     * @param a: The first integer
+     * @param b: The second integer
+     * @return: The sum of a and b
+     */
+    int aplusb(int a, int b) {
+        // write your code here, try to do it without arithmetic operators.
+        int cf = a & b;  
+        int sum = a ^ b;  
+  
+        while (cf) {  
+            a = sum;  
+            b = cf << 1;  
+            cf = a & b;  
+            sum = a ^ b;  
+        } 
+        
+        return sum;
+    }
+};
+
