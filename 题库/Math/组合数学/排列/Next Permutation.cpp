7df917dc6fb6a3a1f9dca90c353ab2c0b860317f
vo01github <hxt2014bbg@icloud.com>
2015-07-08 20:45:25 +0800

    2

---
+++ 题库/Math/组合数学/排列/Next Permutation.cpp
@@ -0,0 +1,110 @@
+﻿
+
+
+// __________________________ Next Permutation __________________________
+/*
+题目来源:
+https://leetcode.com/problems/next-permutation/
+http://www.lintcode.com/zh-cn/problem/next-permutation-ii/
+
+下一个排列
+*/  
+
+
+/*
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+The replacement must be in-place, do not allocate extra memory.
+
+Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+*/
+
+// __________________________ Tags __________________________
+/*
+	 排列，数组
+*/
+
+// __________________________ 实现细节 __________________________ 
+// 代码1-从后往前遍历，
+// 时间复杂度O(n^2),空间复杂度O(1)
+class Solution {
+public:
+    /**
+     * @param nums: a vector of integers
+     * @return: return nothing (void), do not return anything, modify nums in-place instead
+     */
+    void nextPermutation(vector<int> &nums) {
+        // write your code here
+        if (nums.size() < 2) return;   // 只有1个元素的，全排列就是自己。
+        
+        bool isFind = false;
+        int min = nums.size()-2;        // 从后往前遍历,找到一个 min 比 后面的一个元素小，这2个元素之间的区域 重新按照从小到大排列。
+        for(int i = min; i > -1 && isFind == false; --i) {
+            for(int j = nums.size()-1; j > i; --j) {
+                if(nums[j] > nums[i]) {
+                    nums[i] ^= nums[j];
+                    nums[j] ^= nums[i];
+                    nums[i] ^= nums[j];
+                    sort(nums.begin()+i+1, nums.end());     // 交换后，本区间 剩下的元素 重新排列。
+                    isFind = true;
+                    break;
+                }
+            }
+        }
+        
+        if (isFind == false) reverse(nums.begin(),nums.end());      // 给出排列[4,3,2,1]，其下一个排列是[1,2,3,4]
+        
+        return;
+    }
+};
+
+
+
+
+// __________________________ Previous Permutation __________________________
+/*
+题目来源:
+http://www.lintcode.com/zh-cn/problem/previous-permuation/
+
+上一个排列
+*/  
+
+// __________________________ 实现细节 __________________________ 
+// 代码1-还是从后往前遍历，就修改2个地方。 一个 > 改成 < ,一个 从大到小排序。
+// 时间复杂度O(n^2),空间复杂度O(1)
+
+class Solution {
+public:
+    /**
+     * @param nums: An array of integers
+     * @return: An array of integers that's previous permuation
+     */
+    vector<int> previousPermuation(vector<int> &nums) {
+        // write your code here
+        if (nums.size() < 2) return nums;   // 只有1个元素的，全排列就是自己。
+        
+        bool isFind = false;
+        int min = nums.size()-2;        // 从后往前遍历,找到一个 min 比 后面的一个元素小，这2个元素之间的区域 重新按照从大到小排列。
+        for(int i = min; i > -1 && isFind == false; --i) {
+            for(int j = nums.size()-1; j > i; --j) {
+                if(nums[j] < nums[i]) {
+                    nums[i] ^= nums[j];
+                    nums[j] ^= nums[i];
+                    nums[i] ^= nums[j];
+                    sort(nums.begin()+i+1, nums.end(), greater<int>());     // 交换后，本区间 剩下的元素 重新排列，从大到小。
+                    isFind = true;
+                    break;
+                }
+            }
+        }
+        
+        if (isFind == false) reverse(nums.begin(),nums.end());
+        
+        return nums;
+    }
+};
---
+++ 题库/Math/组合数学/排列/Permutation Sequence.cpp
@@ -0,0 +1,74 @@
+﻿
+
+
+// __________________________ Permutation Sequence  __________________________
+/*
+题目来源:
+https://leetcode.com/problems/permutation-sequence/
+http://www.lintcode.com/zh-cn/problem/permutation-sequence/
+
+第k个排列
+*/  
+
+
+/*
+The set [1,2,3,…,n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order,
+We get the following sequence (ie, for n = 3):
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+
+Note: Given n will be between 1 and 9 inclusive.
+
+*/
+
+// __________________________ Tags __________________________
+/*
+	 排列，Math
+*/
+
+// __________________________ 实现细节 __________________________ 
+// 代码1-从后往前遍历，
+// 时间复杂度O(n^2),空间复杂度O(1)
+
+class Solution {
+public:
+    /**
+      * @param n: n
+      * @param k: the kth permutation
+      * @return: return the k-th permutation
+      */
+    string getPermutation(int n, int k) {
+        vector<int> num_pool;       // 把 num_pool 里面的数一个个的取出来，放入 string 中。
+        num_pool.push_back(1);
+       
+        long a[9];                  // 统计每种 n 下面总共有多少个数。
+        a[0] = 1;
+        for (int i = 1; i < n; ++i) {
+           a[i] = a[i-1] * (i+1);
+           num_pool.push_back(i+1);
+        }
+       
+       
+        string ret;
+        --k;    // 把 边界 0 算进去
+        while(n > 0) {
+            long pool = a[n-1]/n;       // 平分这个数
+            int one = k/pool;           // 第 k 个排列 的第1位为多少。
+            int tou = num_pool[one];    // 从num_pool中取出这个数。
+            num_pool.erase(num_pool.begin() + one);  // 然后删掉这个取出的数。
+            ret.push_back(tou + '0');
+            --n;
+            k %= pool;
+        }
+       
+        return ret;
+    }   
+};
