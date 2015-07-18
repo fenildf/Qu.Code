bca3b4bd9078ef118b82ac34c8016e8048943b43
vo01github <hxt2014bbg@icloud.com>
2015-07-18 11:17:50 +0800

    2

---
+++ 题库/阶段间的状态转移/动态规划/Longest Common Subsequence.cpp
@@ -0,0 +1,70 @@
+﻿
+
+
+// __________________________ Longest Common Subsequence __________________________
+/*
+题目来源:
+http://www.lintcode.com/en/problem/longest-common-subsequence/
+
+最长公共子序列
+*/  
+
+
+/*
+Medium Longest Common Subsequence
+
+36% Accepted
+Given two strings, find the longest common subsequence (LCS).
+
+Your code should return the length of LCS.
+
+
+Example
+For "ABCD" and "EDCA", the LCS is "A" (or "D", "C"), return 1.
+
+For "ABCD" and "EACB", the LCS is "AC", return 2.
+
+Clarification
+What's the definition of Longest Common Subsequence?
+
+https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
+http://baike.baidu.com/view/2020307.htm
+
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Dynamic Programming-动态规划;
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1 - 使用循环 - 
+// 时间复杂度O(n*m),空间复杂度O(m*n)。
+// 使用2维数组来记录已经运算过的状态。
+class Solution {
+public:
+    /**
+     * @param A, B: Two strings.
+     * @return: The length of longest common subsequence of A and B.
+     */
+    int longestCommonSubsequence(string A, string B) {
+        // write your code here
+        int memory[100][100];
+        memset(memory,0,sizeof(memory));
+        
+        for (int i = 0; i < A.size(); ++i) {
+            for (int j = 0; j < B.size(); ++j) {
+                if(A[i] == B[j]) {
+                    memory[i+1][j+1] =  memory[i][j] + 1;
+                } else {
+                    memory[i+1][j+1] = max(memory[i+1][j], memory[i][j+1]);
+                }
+            }
+        }
+        
+        return memory[A.size()][B.size()];
+    }
+};
+
+
---
+++ 题库/阶段间的状态转移/动态规划/Longest Common Substring.cpp
@@ -0,0 +1,77 @@
+﻿
+
+
+// __________________________ Longest Common Substring __________________________
+/*
+题目来源:
+http://www.lintcode.com/en/problem/longest-common-substring/
+
+最长公共子串
+*/  
+
+
+/*
+Given two strings, find the longest common substring.
+
+Return the length of it.
+
+
+Example
+Given A = "ABCD", B = "CBCE", return 2.
+
+Note
+The characters in substring should occur continuously in original string. This is different with subsequence.
+
+Challenge
+O(n x m) time and memory.
+
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Dynamic Programming-动态规划;
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1 - 使用暴力法 
+// 时间复杂度O(n*m),空间复杂度O(1)。
+/*  时间优化：
+1： i < A.size()-maxLength; 当后面要查的字符串长度小于已经找到的子串的长度时，没必要遍历下去。
+2： j = max(j, j + k - 1);  如果找到一个很长的子串，则 j 可以跳跃过这段子串的长度遍历。
+*/ 
+class Solution {
+public:    
+    /**
+     * @param A, B: Two string.
+     * @return: the length of the longest common substring.
+     */
+    int longestCommonSubstring(string &A, string &B) {
+        // write your code here
+        
+        int maxLength = 0;
+        
+        for (int i = 0; i < A.size()-maxLength; ++i) {      // 小于已经找到的长度时，没必要找
+            
+            for (int j = 0; j < B.size()-maxLength; ++j) {
+                
+                // i 和 j 确定已经找到的长度
+                int sonLen = 0;
+                for (int k = 0; k+i < A.size() && k+j < B.size(); ++k) {
+                    if(A[i+k] == B[j+k]) {
+                        ++sonLen;
+                    } else {
+                       j = max(j, j + k - 1);     
+                       break;
+                    }
+                }
+                maxLength = max(maxLength, sonLen);
+                sonLen = 0;
+            }
+        }
+        
+        return maxLength;
+    }
+};
+
+
