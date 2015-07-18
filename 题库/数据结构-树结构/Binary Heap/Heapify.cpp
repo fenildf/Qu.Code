806de961e74384f58670fd412200dea2272afb3c
vo01github <hxt2014bbg@icloud.com>
2015-07-18 13:58:52 +0800

    2

---
+++ 题库/数据结构-树结构/Binary Heap/Heapify.cpp
@@ -0,0 +1,79 @@
+﻿
+
+
+// __________________________ Heapify __________________________
+/*
+题目来源:
+http://www.lintcode.com/en/problem/heapify/
+
+堆化
+*/  
+
+
+/*
+Given an integer array, heapify it into a min-heap array.
+
+For a heap array A, A[0] is the root of heap, and for each A[i], A[i * 2 + 1] is the left child of A[i] and A[i * 2 + 2] is the right child of A[i].
+
+Example
+Given [3,2,1,4,5], return [1,2,3,4,5] or any legal heap array.
+
+Challenge
+O(n) time complexity
+
+Clarification
+What is heap?
+
+Heap is a data structure, which usually have three methods: push, pop and top. where "push" add a new element the heap, "pop" delete the minimum/maximum element in the heap, "top" return the minimum/maximum element.
+
+What is heapify?
+Convert an unordered integer array into a heap array. If it is min-heap, for each element A[i], we will get A[i * 2 + 1] >= A[i] and A[i * 2 + 2] >= A[i].
+
+What if there is a lot of solutions?
+Return any of them.
+
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Dynamic Programming-动态规划;
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1 - 使用数组实现堆，依次取出每个数据来放入新建的堆中，然后堆化。 
+// 时间复杂度O(nlogn),空间复杂度O(n)。
+
+class Solution {
+public:
+    /**
+     * @param A: Given an integer array
+     * @return: void
+     */
+    vector<int> heap; 
+    void heapify(vector<int> &nums) {
+        // write your code here
+        for (int i = 0; i < nums.size(); ++i) {
+            push(nums[i]);
+        }
+        nums = heap;
+    }
+    
+    void push(int x) {
+        heap.push_back(x);
+        
+        int p = heap.size() - 1;
+        
+        while(p > 0) {
+            int father = (p-1)/2;   // 计算出 父节点 索引值
+            
+            if (heap[father] < heap[p]) break;
+            
+            swap(heap[father], heap[p]);
+            
+            p = father;
+        }
+    }
+};
+
+
---
+++ 题库/阶段间的状态转移/动态规划/Minimum Path Sum.cpp
@@ -0,0 +1,91 @@
+﻿
+
+
+// __________________________ Minimum Path Sum  __________________________
+/*
+题目来源:
+http://www.lintcode.com/zh-cn/problem/minimum-path-sum/
+https://leetcode.com/problems/minimum-path-sum/
+
+最小路径和
+*/  
+
+
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Dynamic Programming-动态规划;
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1 - 使用二维数组记录 状态值
+// 时间复杂度O(n*m),空间复杂度O(n*m)。
+class Solution {
+public:
+    /**
+     * @param grid: a list of lists of integers.
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    
+    int minPathSum(vector<vector<int> > &grid) {
+        // write your code here
+        int memory[400][400];
+        memset(memory,0,sizeof(memory));   // 
+        
+        if (grid == vector<vector<int>> ()) return 0;   // 空返回 0
+        
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (i == 0) {       // 两个边界要特殊化处理。
+                    memory[i+1][j+1] = grid[i][j] + memory[i+1][j]; 
+                } else if (j == 0) {
+                    memory[i+1][j+1] = grid[i][j] + memory[i][j+1]; 
+                }
+                else {
+                   memory[i+1][j+1] = grid[i][j] + min(memory[i+1][j], memory[i][j+1]); 
+                }
+            }
+        }
+        
+        return memory[grid.size()][grid[0].size()];
+    }
+};
+
+// 代码2 - 使用一维数组记录 状态值，滚动数组的原理
+// 时间复杂度O(n*m),空间复杂度O(m)。
+class Solution {
+public:
+    /**
+     * @param grid: a list of lists of integers.
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    int minPathSum(vector<vector<int> > &grid) {
+        // write your code here
+        vector<int> memory(grid[0].size() + 1, 0);      // 加一行辅助行
+        
+        if (grid == vector<vector<int>> ()) return 0;   // 空返回 0
+        
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (i == 0) {       // 两个边界要特殊化处理。
+                    memory[j+1] = grid[i][j] + memory[j]; 
+                } else if (j == 0) {
+                    memory[j+1] = grid[i][j] + memory[j+1]; 
+                }
+                else {
+                   memory[j+1] = grid[i][j] + min(memory[j], memory[j+1]); 
+                }
+            }
+        }
+        
+        return memory[grid[0].size()];
+    }
+};
+
