3e747ccbaeaf12fff9a31a629546de0c7c7061b0
vo01github <hxt2014bbg@icloud.com>
2015-08-12 18:07:07 +0800

    3

--- HDU题库/1000 A+B Problem.cpp
+++ HDU题库/1000 A+B Problem.cpp
@@ -30,8 +30,6 @@ Sample Input
 Sample Output
 2
  
-
-
 */
 
 // __________________________ Tags __________________________
@@ -49,7 +47,25 @@ Sample Output
 int main()
 {
   int a, b;
-  while (scanf("%d%d", &a, &b) != EOF)
-  printf ("%d\n", a + b);
+  while (scanf("%d%d", &a, &b) != EOF) {
+  	printf ("%d\n", a + b);
+  }
+  
   return 0;
 }
+
+
+/*
+
+scanf("%d %d",&a,&b)返回输入的数据和格式字符串中匹配次数。
+
+当dos或windows中输入ctrl+z（模拟文件结束符EOF）时，scanf返回EOF,
+
+所以当输入终止符ctrl+z时，退出while循环
+
+scanf()函数接收输入数据时，遇以下情况结束一个数据的输入：（不是结束该scanf函数，scanf函数仅在每一个数据域均有数据，并按回车后结束）。
+        ① 遇空格、“回车”、“跳格”键。
+        ② 遇宽度结束。
+        ③ 遇非法输入。
+
+*/
---
+++ HDU题库/1090 A+B for Input-Output Practice (II).cpp
@@ -0,0 +1,60 @@
+﻿
+
+
+// __________________________ 1090 A+B for Input-Output Practice (II)  __________________________
+/*
+题目来源:
+http://acm.hdu.edu.cn/showproblem.php?pid=1090
+
+*/  
+
+
+/*
+Problem Description
+Your task is to Calculate a + b.
+ 
+
+Input
+Input contains an integer N in the first line, and then N lines follow. Each line consists of a pair of integers a and b, separated by a space, one pair of integers per line. 
+ 
+
+Output
+For each pair of input integers a and b you should output the sum of a and b in one line, and with one line of output for each line in input. 
+ 
+
+Sample Input
+2
+1 5
+10 20
+ 
+
+Sample Output
+6
+30
+ 
+*/
+
+// __________________________ Tags __________________________
+/*
+	 
+*/
+
+// __________________________ 实现细节 __________________________ 
+
+// 代码1
+// 时间复杂度O(),空间复杂度O()
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
