cdd89cdfdf4b6ced2cc0b1abda021d83d594e1b1
vo01github <hxt2014bbg@icloud.com>
2015-09-03 15:15:51 +0800

    HDU

---
+++ 题库/二分法/3Sum.cpp
@@ -0,0 +1,224 @@
+﻿
+
+
+// __________________________ 3Sum   __________________________
+/*
+题目来源:
+https://leetcode.com/problems/3sum/
+http://www.lintcode.com/zh-cn/problem/3-sum/
+
+三数之和
+*/  
+
+
+/*
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+The solution set must not contain duplicate triplets.
+    For example, given array S = {-1 0 1 2 -1 -4},
+
+    A solution set is:
+    (-1, 0, 1)
+    (-1, -1, 2)
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Binary Search
+*/
+
+// __________________________ 实现细节 __________________________ 
+// 代码1 - 二叉搜索法
+// 时间复杂度O(?),     在 leetcode 上面会因为超时，而没有办法 AC的。
+// 空间复杂度O(n)     
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        // write your code here
+        vector<vector<int>> resPool;
+        for (int i = 0; i < nums.size(); ++i) {
+            int target = 0 - nums[i];
+            vector<int> numSon = nums;
+            numSon.erase(numSon.begin() + i);    // 把第1个元素从池子里面拿出去
+            vector<vector<int>> restultPool = twoSum(numSon, target);
+            for (int j = 0; j < restultPool.size(); ++j) {
+                vector<int> res = restultPool[j];
+                if (res.size() == 3) {
+                    // 检查已存 数组 里面是否有重复的数组。  
+                    if (!isContainVector(resPool, res)) resPool.push_back(res);
+                }
+            }
+        }
+        
+        return resPool;
+    }
+    
+    // 所有传入的对比数组 必须是 已经排序的。
+    bool isContainVector(vector<vector<int>> &pool, vector<int> &nums) {
+        for (int j = 0; j < pool.size(); ++j) {
+            if(isTwoVectorEqual(pool[j], nums)) {
+               return true;
+            }
+        }
+        
+        return false;   // 说明 pool 里面没有包含 nums
+    }
+    
+    // 所有传入的对比数组 必须是 已经排序的。
+    bool isTwoVectorEqual(vector<int> &nums1, vector<int> &nums2) {
+        if (nums1.size() != nums2.size()) return false;
+        for (int i = 0; i < nums1.size(); ++i) {
+            if (nums1[i] != nums2[i]) return false;
+        }
+        
+        return true;
+    }
+    
+    
+    void deleteNum(vector<int> &nums, int goal) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == goal) {
+                nums.erase(nums.begin() + i);
+            }
+        }
+    }
+    
+    vector<vector<int>> twoSum(vector<int>& nums, int target) {
+        vector<int> pool = nums;
+        
+        sort(pool.begin(), pool.end());
+        
+        int i;
+        vector<vector<int>> restultPool;
+        
+        for (i = 0; i < nums.size(); ++i) {
+            int midTar;
+            midTar = target - pool[i];
+            vector<int> poolSon = pool;
+            poolSon.erase(poolSon.begin() + i);       // 把第2个元素从池子里面拿出去
+            if (binary_search(poolSon.begin(), poolSon.end(), midTar)) {
+                vector<int> res;
+                res.push_back(midTar);
+                res.push_back(pool[i]);
+                res.push_back(-target);
+                sort(res.begin(), res.end());
+                restultPool.push_back(res);
+            }
+        }
+
+        return restultPool;
+    }
+};
+
+
+// 代码1 - 
+// 时间复杂度O(?),     进行了优化的。 还是不行。
+// 空间复杂度O(n)  
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        // 记录返回结果的池子
+        vector<vector<int>> resPool;
+        
+        sort(nums.begin(), nums.end()); 
+        
+        // 从原数组中 只每样挑一个出来检查。
+        vector<int> removeRepeat = nums;       
+        removeRepeat.erase(unique(removeRepeat.begin(), removeRepeat.end()), removeRepeat.end());
+        
+        // 开始检测
+        for (int i = 0; i < removeRepeat.size(); ++i) {
+            int target = 0 - removeRepeat[i];
+            vector<int> numSon = nums;
+            deleteNum(numSon, removeRepeat[i]);     // 把第1个元素从池子里面拿出去
+   
+            vector<vector<int>> restultPool = twoSum(numSon, target);
+            for (int j = 0; j < restultPool.size(); ++j) {
+                vector<int> res = restultPool[j];
+                if (res.size() == 3) {
+                    // 检查已存 数组 里面是否有重复的数组。  
+                    if (!isContainVector(resPool, res)) resPool.push_back(res);
+                }
+            }
+        }
+        
+        return resPool;
+    }
+    
+    vector<vector<int>> twoSum(vector<int>& nums, int target) {
+        vector<int> pool = nums;
+        
+        //sort(pool.begin(), pool.end());
+        vector<int> removeRepeat = nums;       
+        removeRepeat.erase(unique(removeRepeat.begin(), removeRepeat.end()), removeRepeat.end());
+        
+        int i;
+        vector<vector<int>> restultPool;
+        
+        for (i = 0; i < removeRepeat.size(); ++i) {
+            int midTar;
+            midTar = target - removeRepeat[i];
+            vector<int> poolSon = pool;
+            deleteNum(poolSon, removeRepeat[i]);
+            //poolSon.erase(poolSon.begin() + i);       // 把第2个元素从池子里面拿出去
+            
+            if (binary_search(poolSon.begin(), poolSon.end(), midTar)) {
+                vector<int> res;
+                res.push_back(midTar);
+                res.push_back(pool[i]);
+                res.push_back(-target);
+                sort(res.begin(), res.end());
+                restultPool.push_back(res);
+            }
+        }
+        
+        return restultPool;
+    }
+    
+    // 所有传入的对比数组 必须是 已经排序的。
+    bool isContainVector(vector<vector<int>> &pool, vector<int> &nums) {
+        for (int j = 0; j < pool.size(); ++j) {
+            if(isTwoVectorEqual(pool[j], nums)) {
+               return true;
+            }
+        }
+        
+        return false;   // 说明 pool 里面没有包含 nums
+    }
+    
+    // 所有传入的对比数组 必须是 已经排序的。
+    bool isTwoVectorEqual(vector<int> &nums1, vector<int> &nums2) {
+        if (nums1.size() != nums2.size()) return false;
+        for (int i = 0; i < nums1.size(); ++i) {
+            if (nums1[i] != nums2[i]) return false;
+        }
+        
+        return true;
+    }
+    
+    // 删除发现的第一个元素。
+    void deleteNum(vector<int> &nums, int goal) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == goal) {
+                nums.erase(nums.begin() + i);
+            }
+        }
+    }
+};
+
+// __________________________    __________________________
+/*
+题目来源:
+
+
+
+*/  
+
+
+/*
+
+*/
+
+class Solution {
---
+++ 题库/二分法/Two Sum.cpp
@@ -0,0 +1,115 @@
+﻿
+
+
+// __________________________ Two Sum   __________________________
+/*
+题目来源:
+http://www.lintcode.com/submission/1467308/
+https://leetcode.com/problems/two-sum/
+
+两数之和
+*/  
+
+
+/*
+Given an array of integers, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+*/
+
+// __________________________ Tags __________________________
+/*
+	 Binary Search
+*/
+
+// __________________________ 实现细节 __________________________ 
+// 代码1 - 暴力搜索法
+// 时间复杂度O(N*N),     在 leetcode 上面会因为超时，而没有办法 AC的。
+// 空间复杂度O(n)     
+class Solution {
+public:
+    /*
+     * @param numbers : An array of Integer
+     * @param target : target = numbers[index1] + numbers[index2]
+     * @return : [index1+1, index2+1] (index1 < index2)
+     */
+    vector<int> twoSum(vector<int> &nums, int target) {
+        // write your code here
+        
+        vector<int> pool;
+        for (int i = 0; i < nums.size(); ++i) {
+            int midTar;
+            midTar = target - nums[i];
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (midTar == nums[j]) {
+                    pool.push_back(i+1);
+                    pool.push_back(j+1);
+                    
+                    return pool;
+                }
+            }
+        }
+    }
+};
+
+
+// 代码2 - 使用二分法
+// 时间复杂度O(nlog(n)排序 + nlog(n)二分查找 + n确定位置),
+// 空间复杂度O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> pool = nums;
+        
+        sort(pool.begin(), pool.end());
+        
+        int i;
+        for (i = 0; i < nums.size(); ++i) {
+            int midTar;
+            midTar = target - pool[i];
+            if (binary_search(pool.begin(), pool.end(), midTar)) {
+                break;
+            }
+        }
+        
+        // 找到要查找的数了。
+        int a = pool[i];
+        int b = target - pool[i];
+        
+        // 接下来确定 A 和 B 在 nums中的位置。
+        vector<int> res;
+        for(int j = 0; j < nums.size(); ++j) {
+            if(nums[j] == a || nums[j] == b) {
+                if (res.size() == 0) {
+                    res.push_back(j+1);
+                } else if (nums[j] + nums[res[0]-1] == target) {    // 找对值了
+                    res.push_back(j+1);
+                    break;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+// __________________________    __________________________
+/*
+题目来源:
+
+
+
+*/  
+
+
+/*
+
+*/
+
+class Solution {
